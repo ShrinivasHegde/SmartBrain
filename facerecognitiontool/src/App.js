46c0f20822f11da3f496aeaa3fac04725dc1538b
Shrinivas Hegde <mkshrinivashegde@gmail.com>
2023-04-13 21:17:50 +0530

    Deplyoing on Render

--- facerecognitiontool/src/App.js
+++ facerecognitiontool/src/App.js
@@ -103,7 +103,7 @@ class App extends Component {
     this.setState({imgUrl:this.state.input})
 
     //console.log("click")
-    fetch('http://localhost:3000/imageUrl',{
+    fetch('https://smartbrainappapi.onrender.com/imageUrl',{
             method: 'post',
             headers:{'Content-Type':'application/json'},
             body:JSON.stringify({
@@ -113,7 +113,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if(response){
-          fetch('http://localhost:3000/image',{
+          fetch('https://smartbrainappapi.onrender.com/image',{
             method: 'put',
             headers:{'Content-Type':'application/json'},
             body:JSON.stringify({
